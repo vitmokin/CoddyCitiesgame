d0be8815a635d9d2b19687b6ec39c4e354b6908a
Ayan Tagieva <tagieva.ayan@gmail.com>
2021-10-31 20:43:57 +0300

    начала делать проверку на повторения

--- goroda.py
+++ goroda.py
@@ -4,6 +4,7 @@ igrok1=str(input('Введите название города'))
 dlina1=igrok1[-1]
 igrok2=str(input('Введите название города'))
 dlina2=igrok2[0]
+answer_list=[igrok1,igrok2]
 while dlina1 == dlina2:
     print('далее')
     igrok1=str(input('Введите название города'))
@@ -12,3 +13,14 @@ while dlina1 == dlina2:
     dlina2=igrok2[0]
 else:
     print('none')
+#proverka povtoreniy
+for i in answer_list:
+    if i == igrok1:
+        print('obnaruzheno povtorenie')
+    else:
+        print('далее')
+for i in answer_list:
+    if i == igrok2:
+        print('obnaruzheno povtorenie')
+    else:
+        print('далее')    
