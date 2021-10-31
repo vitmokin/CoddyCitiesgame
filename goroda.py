53a0d515bc133eca3f9e5b5b014af10b4a4d74b3
Ayan Tagieva <tagieva.ayan@gmail.com>
2021-10-31 18:33:58 +0300

    найдите десять отличий

--- goroda.py
+++ goroda.py
@@ -1,14 +1,14 @@
 ##file for Ayan
 ##print("Hello, Ayan")
-def goroda():
+igrok1=str(input('Введите название города'))
+dlina1=igrok1[-1]
+igrok2=str(input('Введите название города'))
+dlina2=igrok2[0]
+while dlina1 == dlina2:
+    print('далее')
     igrok1=str(input('Введите название города'))
     dlina1=igrok1[-1]
     igrok2=str(input('Введите название города'))
     dlina2=igrok2[0]
-    if dlina1 == dlina2:
-        a=dlina1 == dlina2
-        print('далее')
-    else:
-        print('none')
-    return a
-print(goroda)
+else:
+    print('none')
