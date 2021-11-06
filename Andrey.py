887249fec8834f1a4c0fd3157bafaeda29006108
Vitaliy Mokin <v.mokin@group-ib.com>
2021-11-06 15:20:05 +0300

    Код теперь выглядит красиво (согласно стандартам)

--- Andrey.py
+++ Andrey.py
@@ -1,30 +1,29 @@
-#file for Andrey
+# file for Andrey
 import time
 
+
 def Таймер():
-    T=30
-    while T>0:
-        T-=10
+    T = 30
+    while T > 0:
+        T -= 10
         time.sleep(10)
-        print('осталось {} секунд'.format(T))    
+        print('осталось {} секунд'.format(T))
     print('ты проиграл')
-
-
-
 
 
 def game():
     while True:
-        a=input('Город - ')
-        last_letter=a[len(a)-1]
-        a=input('Напишите город ')
-        #что-то придумаю
+        a = input('Город - ')
+        last_letter = a[len(a)-1]
+        a = input('Напишите город ')
+        # что-то придумаю
         if last_letter.upper() == a[0]:
             pass
         else:
-            print('Буква, на которую оканчивается прошлый город не совпадает с первой буквой нового города')
+            print(
+                'Буква, на которую оканчивается прошлый город не совпадает с первой буквой нового города')
             pass
-        last_letter=a[len(a)-1]
-            
+        last_letter = a[len(a)-1]
+
 
 print(game())
--- Daniil.py
+++ Daniil.py
@@ -1,39 +1,39 @@
-#file for Daniil
-cities_list=["Москва","Кострома","Сочи","Челябинск"]
-city_play_lista=[]
-temp=0
-victorypoint1=0
-victorypoint2=0
-lastletter=0
-firstletter=0
-trigh_max=int(input("Введите количество ходов которых сделает каждый игрок"))
-trigh_max=trigh_max*2
-if trigh_max>len(cities_list):
+# file for Daniil
+cities_list = ["Москва", "Кострома", "Сочи", "Челябинск"]
+city_play_lista = []
+temp = 0
+victorypoint1 = 0
+victorypoint2 = 0
+lastletter = 0
+firstletter = 0
+trigh_max = int(input("Введите количество ходов которых сделает каждый игрок"))
+trigh_max = trigh_max*2
+if trigh_max > len(cities_list):
     print("слишком много попыток,нет столько городов")
     SystemError
 while True:
-    temp=str(input("первый игрок введите название города"))
-    lastletter=temp[-1]
-    cities_list.insert(0,temp)
-    check=cities_list.count(temp)
-    if check!=1:
+    temp = str(input("первый игрок введите название города"))
+    lastletter = temp[-1]
+    cities_list.insert(0, temp)
+    check = cities_list.count(temp)
+    if check != 1:
         cities_list.pop(0)
-        city_play_lista.insert(0,temp)
-        victorypoint1=victorypoint1+1
-        trigh=trigh+1
+        city_play_lista.insert(0, temp)
+        victorypoint1 = victorypoint1+1
+        trigh = trigh+1
     else:
         cities_list.pop(0)
         print("такого города нет(попробуйте иначать с заглавной буквы)")
-        trigh=trigh+1
+        trigh = trigh+1
 print(secondpersonpain)
 while True:
     print(firstpersonpain)
     print(secondpersonpain)
-    if trigh==trigh_max:
-        if victorypoint1>victorypoint2:
+    if trigh == trigh_max:
+        if victorypoint1 > victorypoint2:
             print("первый игрок победил")
             break
-        elif victorypoint1<victorypoint2:
+        elif victorypoint1 < victorypoint2:
             print("второй игрок победил")
             break
         else:
@@ -41,68 +41,57 @@ while True:
             break
 
 
-
-
-
-
-def firstpersonpain(temp,cities_list,city_play_lista,victorypoint1,lastletter):
-    temp=str(input("первый игрок введите название города"))
-    firstletter=temp[0]
-    firstletter=firstletter.upper()
-    cities_list.insert(0,temp)
-    check=cities_list.count(temp)
-    if check!=1:
+def firstpersonpain(temp, cities_list, city_play_lista, victorypoint1, lastletter):
+    temp = str(input("первый игрок введите название города"))
+    firstletter = temp[0]
+    firstletter = firstletter.upper()
+    cities_list.insert(0, temp)
+    check = cities_list.count(temp)
+    if check != 1:
         cities_list.pop(0)
-        city_play_lista.insert(0,temp)
-        check2=city_play_lista.count(temp)
-        if check2!=1:
+        city_play_lista.insert(0, temp)
+        check2 = city_play_lista.count(temp)
+        if check2 != 1:
             city_play_lista.pop(0)
         print("уже было")
-        trigh=trigh+1
-        elif lastletter==firstletter:
-            trigh=trigh+1
-            victorypoint1=victorypoint1+1
-            lastletter=firstletter
+        trigh = trigh+1
+        elif lastletter == firstletter:
+            trigh = trigh+1
+            victorypoint1 = victorypoint1+1
+            lastletter = firstletter
         else:
-            trigh=trigh+1
-            print("первая буква этого слова и последняя буква прошлого слова не совподают")
+            trigh = trigh+1
+            print(
+                "первая буква этого слова и последняя буква прошлого слова не совподают")
     else:
         cities_list.pop(0)
         print("такого города нет(попробуйте иначать с заглавной буквы)")
-        trigh=trigh+1
-        
+        trigh = trigh+1
 
 
-
-def secondpersonpain(temp,cities_list,city_play_lista,victorypoint2,lastletter):
-    temp=str(input("введите название города"))
-    firstletter=temp[0]
-    firstletter=firstletter.upper()
-    cities_list.insert(0,temp)
-    check=cities_list.count(temp)
-    if check!=1:
+def secondpersonpain(temp, cities_list, city_play_lista, victorypoint2, lastletter):
+    temp = str(input("введите название города"))
+    firstletter = temp[0]
+    firstletter = firstletter.upper()
+    cities_list.insert(0, temp)
+    check = cities_list.count(temp)
+    if check != 1:
         cities_list.pop(0)
-        city_play_lista.insert(0,temp)
-        check2=city_play_lista.count(temp)
-        if check2!=1:
+        city_play_lista.insert(0, temp)
+        check2 = city_play_lista.count(temp)
+        if check2 != 1:
             city_play_lista.pop(0)
         print("уже было")
-        trigh=trigh+1
-        elif lastletter==firstletter:
-            trigh=trigh+1
-            victorypoint2=victorypoint2+1
-            lastletter=firstletter
+        trigh = trigh+1
+        elif lastletter == firstletter:
+            trigh = trigh+1
+            victorypoint2 = victorypoint2+1
+            lastletter = firstletter
         else:
-            trigh=trigh+1
-            print("первая буква этого слова и последняя буква прошлого слова не совподают")
+            trigh = trigh+1
+            print(
+                "первая буква этого слова и последняя буква прошлого слова не совподают")
     else:
         cities_list.pop(0)
         print("такого города нет(попробуйте иначать с заглавной буквы)")
-        trigh=trigh+1
-    
-        
-
-
-
-                
-   
+        trigh = trigh+1
