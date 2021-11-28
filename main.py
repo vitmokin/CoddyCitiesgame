b07a10dd695e9bf089cb33bd8d373807fc6aeda5
Vitaliy Mokin <v.mokin@group-ib.com>
2021-11-28 18:32:21 +0300

    perf_counter -> time

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from time import perf_counter
+from time import time
 
 
 def timer():
-    now = perf_counter()
+    now = time()
     flag20 = True
     flag10 = True
-    while perf_counter() - now < 30:
-        if perf_counter() - now > 10 and flag20:
+    while time() - now < 30:
+        if time() - now > 10 and flag20:
             print("Осталось 20 секунд")
             flag20 = False
-        if perf_counter() - now > 20 and flag10:
+        if time() - now > 20 and flag10:
             print("Осталось 10 секунд")
             flag10 = False
     print('Вы проиграли')
