d5107c7cf6a7dc11cf59c390907de32526d5f317
Ayan Tagieva <tagieva.ayan@gmail.com>
2021-11-28 18:08:57 +0300

    я что то сделала

--- Ayan.py
+++ Ayan.py
@@ -1,40 +1,46 @@
-###################################################
-def simple_dimple():
+def simple_game():
     igrok1 = input('Введите название города')
+    dlina1 = igrok1[-1]
+    while dlina1 == dlina2:
+                igrok1 = input('Введите название города')
+        for i in answer_list:
+            if i == igrok1:
+                print('obnaruzheno povtorenie')
+            else:
+                print('далее')
     igrok2 = input('Введите название города')
+    dlina2 = igrok2[0]
+            igrok2 = input('Введите название города')
+        for i in answer_list:
+            if i == igrok2:
+                print('obnaruzheno povtorenie')
+            else:
+                print('далее')
     answer_list = [igrok1, igrok2]
-print(simple_dimple())
-###################################################
-def uvelichenie_bukvi(answer_list):
+#
     for i in answer_list:
         if i == i[-1]:
-            i.upper() 
-print(uvelichenie_bukvi(answer_list))
-###################################################
-def pop_it(igrok1,answer_list):
+            i.upper()
+            print(i)
+        else:
+            print("ошибка")
+#
     while dlina1 == dlina2:
         print('далее')
         igrok1 = input('Введите название города')
-        dlina1 = igrok1[-1]
         for i in answer_list:
             if i == igrok1:
                 print('obnaruzheno povtorenie')
             else:
                 print('далее')
         igrok2 = input('Введите название города')
-        dlina2 = igrok2[0]
         for i in answer_list:
             if i == igrok2:
                 print('obnaruzheno povtorenie')
             else:
                 print('далее')
-        return answer_list
-        print(proverka(igrok1, dlina1, dlina2, answer_list))
-
+print(simple_game())
 # Логику не проверял, пока ты не доделала
-# Внутри simple_dimple() опять строка не используется 
-# 15 строка - переменных dlina1 и dlina2 нет в функции
-# 32 строка - всё плохо с отступами 
-
+    
 #####################################################
 # написал функцию - протестировал сначала отдельно её, потом её связь с остальными функциями
