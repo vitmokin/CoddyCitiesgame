bf5a7879587d1ff696544f62334d68a8bdd60d81
Ayan Tagieva <tagieva.ayan@gmail.com>
2021-12-26 17:47:25 +0300

    mdijeifjeijw

--- Ayan.py
+++ Ayan.py
@@ -1,20 +1,26 @@
 def slovo1():
     slovo1=input('ваше слово')
     return slovo1
+slovo1()
 def posl_bukva(slovo1):
     posl_bukva=slovo1[-1]
     return posl_bukva
+posl_bukva(slovo1)
 def slovo2():
     slovo2=input('ваше слово')
     return slovo2
+slovo2()
 def perv_bukva(slovo2):
     perv_bukva=slovo2[0]
     return perv_bukva
+perv_bukva(slovo2)
 def answer_list(slovo1,slovo2):
     answer_list=[slovo1,slovo2]
+answer_list(slovo1,slovo2)
 def uv_bkv(posl_bukva):     
     uvelich_bkv=posl_bukva.capitalize()
     return uvelich_bkv
+uv_bkv(posl_bukva)
 def game(uvelich_bkv,perv_bukva):                                                                  
     if uvelich_bkv != perv_bukva:                                                                         
         print('stop')                                                                              
@@ -25,6 +31,7 @@ def game(uvelich_bkv,perv_bukva):
         perv_bukva(slovo2)
         answer_list(slovo1,slovo2)
     return answer_list
+game(uvelich_bkv,perv_bukva)
 def povtorenie(answer_list,slovo1,slovo2):
     for slv in answer_list:
         if slv == slovo1:
@@ -38,4 +45,4 @@ def povtorenie(answer_list,slovo1,slovo2):
             perv_bukva(slovo2)
     answer_list(slovo1,slovo2)     
     return answer_list
-
+povtorenie(answer_list,slovo1,slovo2)
