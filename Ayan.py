e1c281f97d3bfd53b01552ccd340aee730600de4
Ayan Tagieva <tagieva.ayan@gmail.com>
2021-12-26 17:10:05 +0300

    def

--- Ayan.py
+++ Ayan.py
@@ -1,43 +1,41 @@
-def simple_game():         
-        slovo1=input('ваше слово')           
-        posl_bukva=slovo1[-1]
-        return posl_bukva
-        return slovo1
-simple_game()
+def slovo1():
+    slovo1=input('ваше слово')
+    return slovo1
+def posl_bukva(slovo1):
+    posl_bukva=slovo1[-1]
+    return posl_bukva
+def slovo2():
+    slovo2=input('ваше слово')
+    return slovo2
+def perv_bukva(slovo2):
+    perv_bukva=slovo2[0]
+    return perv_bukva
+def answer_list(slovo1,slovo2):
+    answer_list=[slovo1,slovo2]
 def uv_bkv(posl_bukva):     
-        uvelich_bkv=posl_bukva.capitalize()
-        return uvelich_bkv
-uv_bkv(posl_bukva)
-
-def simple_game2(slovo1):                   
-        slovo2=input('ваше слово')                                                             
-        perv_bukva=slovo2[0]                                                            
-        answer_list=[slovo1,slovo2]
-        return slovo2
-        return perv_bukva
-        return answer_list
-simple_game2(slovo1)
+    uvelich_bkv=posl_bukva.capitalize()
+    return uvelich_bkv
 def game(uvelich_bkv,perv_bukva):                                                                  
-        if uvelich_bkv != perv_bukva:                                                                         
-                print('stop')                                                                              
-        while uvelich_bkv == perv_bukva:                                                                  
-                simple_game(posl_bukva)
-                simple_game2(slovo1)                               
-                if uvelich_bkv != perv_bukva:                                                                  
-                        print('stop')                                                                                                                              
-                simple_game2(slovo1)
-                if uvelich_bkv != perv_bukva:                                                                  
-                        print('stop')
-game(uvelich_bkv,perv_bukva)
+    if uvelich_bkv != perv_bukva:                                                                         
+        print('stop')                                                                              
+    while uvelich_bkv == perv_bukva:
+        slovo1()
+        posl_bukva(slovo1)
+        slovo2()
+        perv_bukva(slovo2)
+        answer_list(slovo1,slovo2)
+    return answer_list
 def povtorenie(answer_list,slovo1,slovo2):
-        for slv in answer_list:
-                if slv == slovo1:
-                        print('повторение,повторите ход')
-                        simple_game(posl_bukva)
-        for slv in answer_list:
-                if slv == slovo2:
-                        print('повторение,повторите ход')
-                        simple_game2(slovo1)
-povtorenie(answer_list,slovo1,slovo2)
-#
+    for slv in answer_list:
+        if slv == slovo1:
+            print('повторение,повторите ход')
+            slovo1()
+            posl_bukva(slovo1)
+    for slv in answer_list:
+        if slv == slovo2:
+            print('повторение,повторите ход')
+            slovo2()
+            perv_bukva(slovo2)
+    answer_list(slovo1,slovo2)     
+    return answer_list
 
