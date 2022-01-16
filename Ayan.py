0b0f5c245426c7d1991ed6eb62d74bc690afe6ea
Ayan Tagieva <tagieva.ayan@gmail.com>
2022-01-16 17:29:36 +0300

    код работает,но не работает

--- Ayan.py
+++ Ayan.py
@@ -1,27 +1,13 @@
-def slovo1():
-    slovo1=input('ваше слово')
-    return slovo1
-slovo1()
-def posl_bukva(slovo1):
-    posl_bukva=slovo1[-1]
-    return posl_bukva
-posl_bukva(slovo1)
-def slovo2():
-    slovo2=input('ваше слово')
-    return slovo2
-slovo2()
-def perv_bukva(slovo2):
-    perv_bukva=slovo2[0]
-    return perv_bukva
-perv_bukva(slovo2)
-def answer_list(slovo1,slovo2):
-    answer_list=[slovo1,slovo2]
-answer_list(slovo1,slovo2)
+slovo1=input('ваше слово')
+posl_bukva=slovo1[-1]
+slovo2=input('ваше слово')
+perv_bukva=slovo2[0]
+answer_list=[slovo1,slovo2]
 def uv_bkv(posl_bukva):     
-    uvelich_bkv=posl_bukva.capitalize()
-    return uvelich_bkv
+    bkva=posl_bukva.capitalize()
+    return bkva
 uv_bkv(posl_bukva)
-def game(uvelich_bkv,perv_bukva):                                                                  
+def game(bkva,perv_bukva):                                                                  
     if uvelich_bkv != perv_bukva:                                                                         
         print('stop')                                                                              
     while uvelich_bkv == perv_bukva:
@@ -31,7 +17,7 @@ def game(uvelich_bkv,perv_bukva):
         perv_bukva(slovo2)
         answer_list(slovo1,slovo2)
     return answer_list
-game(uvelich_bkv,perv_bukva)
+game(bkva,perv_bukva)
 def povtorenie(answer_list,slovo1,slovo2):
     for slv in answer_list:
         if slv == slovo1:
