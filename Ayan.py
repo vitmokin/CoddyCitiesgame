c52c671483b600952f264f22f9aad221ac5a9fb5
Ayan Tagieva <tagieva.ayan@gmail.com>
2022-02-20 17:28:13 +0300

    вот

--- Ayan.py
+++ Ayan.py
@@ -1,21 +1,42 @@
-gorod1=input("город 1")
-posl_bkv=gorod1[-1]
-gorod2=input("город 2")
-perv_bkv=gorod2[0]
-answer_list=[gorod1,gorod2]
-nuzhnaya_vesh=answer_list[-1]
-def proverka(posl_bkv,perv_bkv,gorod1,gorod2):
-        gorod1=input("город 1")
-        posl_bkv=gorod1[-1]
-        gorod2=input("город 2")
-        perv_bkv=gorod2[0]
-        answer_list=[gorod1,gorod2]
-def sama_igra(posl_bkv,perv_bkv):
-    while True:
-        if posl_bkv == perv_bkv:
-            print("хорошо")    
-            proverka(posl_bkv,perv_bkv,gorod1,gorod2)
-        if posl_bkv != perv_bkv:
-            print("не та буква,кретин")
-            proverka(posl_bkv,perv_bkv,gorod1,gorod2)
-sama_igra(posl_bkv,perv_bkv)
+def input_city1():
+    gorod1=input('введите название города')
+    return gorod1
+gorod1=input_city1()
+
+def posl_bkv0(gorod1):
+    posl_bkv0=gorod1[-1]
+    posl_bkv=posl_bkv0.capitalize()
+    return posl_bkv
+posl_bkv=posl_bkv0(gorod1)
+
+def input_city_simple():
+    gorod_simple=input('введите название города')
+    return gorod_simple
+gorod_simple=input_city_simple()
+
+def posl_bkv_s(gorod_simple):
+    posl_bkv1=gorod_simple[-1]
+    posl_bkv_s=posl_bkv1.capitalize()
+    return posl_bkv
+posl_bkv_s=posl_bkv_s(gorod_simple)
+
+def perv_bkv(gorod_simple):
+    perv_bkv=gorod_simple[0]
+    return perv_bkv
+perv_bkv = perv_bkv(gorod_simple)
+
+def igra(posl_bkv,perv_bkv,posl_bkv_s):
+    bad_atempts = 0
+    if posl_bkv != perv_bkv:
+        bad_atempts=bad_atempts+1
+        print(bad_atempts)
+    else:
+        input_city_simple()
+    while posl_bkv_s != perv_bkv:
+        bad_atempts=bad_atempts+1
+    else:
+        input_city_simple()
+        print(bad_atempts)
+    return bad_atempts
+bad_atempts = igra(posl_bkv,perv_bkv,posl_bkv_s)
+
